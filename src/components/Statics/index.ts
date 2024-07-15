bd5272b0d82d184ee1c6f0fa40f44374865a27e5
morteza-k-a <68350615+morteza-k-a@users.noreply.github.com>
2024-07-16 01:54:09 +0430

    changed static policy, added types

---
+++ src/components/Statics/index.ts
@@ -0,0 +1,13 @@
+import { staticsType } from "../types"
+
+const statics : staticsType = {
+    firstPage:{
+        mainImage:'/static/firstPage/main.jpg',
+        gallery:{
+            image1: 'static/firstPage/gallery1.jpg',
+            image2: 'static/firstPage/gallery2.jpg',
+            image3: 'static/firstPage/gallery3.jpg'
+        }
+    }
+}
+export default statics 
---
+++ src/components/types/index.ts
@@ -0,0 +1,19 @@
+interface staticsType {
+  icon?: string;
+  companyIcon?: string;
+  firstPage?: {
+    mainImage?: string;
+    gallery?: {
+      image1?: string;
+      image2?: string;
+      image3?: string;
+      image4?: string;
+    };
+    upcoming?: {
+      watch?: string;
+      instrument?: string;
+    };
+  };
+}
+
+export type { staticsType };
